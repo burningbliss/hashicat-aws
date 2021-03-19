589ab42573e5ed04bd75ff2540527e3f25aaafe1
Darrell Payne <dpayne211@gmail.com>
2021-03-19 19:50:04 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
+module "s3-bucket" {
+  source  = "app.terraform.io/darrellpayne-training/s3-bucket/aws"
+  version = "1.21.0"
+  
+  # insert required variables here
+  bucket_prefix = "darrell-payne"
 }
